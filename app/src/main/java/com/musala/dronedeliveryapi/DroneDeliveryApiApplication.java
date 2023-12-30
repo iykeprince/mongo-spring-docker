4c83e51704a46bc9480652ce21149f06c86dc243
Israel Okorafor <iykeprince48@gmail.com>
2023-12-30 20:41:53 +0100

    initialize maven project

---
+++ app/src/main/java/com/musala/dronedeliveryapi/DroneDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.musala.dronedeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/musala/dronedeliveryapi/DroneDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.dronedeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
